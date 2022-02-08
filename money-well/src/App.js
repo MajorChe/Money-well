9c4b90d2f1c9420fcba74ecb5e8d04ed04f35c9d
MajorChe <charithnanduri03@gmail.com>
2022-02-08 00:58:17 -0500

    add all routes

--- money-well/src/App.js
+++ money-well/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./home/Home";
+import Login from "./login/Login";
+import Signup from "./signup/Signup";
 
 const App = () => {
   return (
-  <div>
-    <h1>hello</h1>
-  </div>);
+    <>
+    <h1>Welcome</h1>
+    <Link to="/login">Login</Link>
+    <Link to="/signup">Sign up</Link>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
-
---
+++ money-well/src/signup/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Signup.module.css"
+const Signup = () => {
+  return (
+    <div className={styles.main}>
+      <h1>This is sign up page</h1>
+    </div>
+  );
+};
+
+export default Signup;
