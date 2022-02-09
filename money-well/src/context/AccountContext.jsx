42a241501fb31f3886016d8ded9c1743991a8a76
MajorChe <charithnanduri03@gmail.com>
2022-02-08 19:31:38 -0500

    add persistant auth state on refresh

--- money-well/src/context/AccountContext.jsx
+++ money-well/src/context/AccountContext.jsx
@@ -1,9 +1,12 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { projectMoneyWellAuth } from "../firebase/Config";
 
 export const AccountContext = createContext();
 
 export const authReducer = (state, action) => {
   switch (action.type) {
+    case "LOGGEDIN":
+      return { ...state, user: action.payload, loggedIn: true };
     case "LOGIN":
       return { ...state, user: action.payload };
     case "LOGOUT":
@@ -16,7 +19,16 @@ export const authReducer = (state, action) => {
 export const AccountContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
+    loggedIn: false,
   });
+
+  useEffect(() => {
+    const unsub = projectMoneyWellAuth.onAuthStateChanged((user) => {
+      dispatch({ type: "LOGGEDIN", payload: user });
+      unsub(); // this will make this function run only once and not when there is a state change
+    });
+  }, []);
+
   console.log("account context state", state);
   return (
     <AccountContext.Provider value={{ ...state, dispatch }}>
