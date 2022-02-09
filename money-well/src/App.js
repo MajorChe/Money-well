b161f1eebab7bef9bccdd539326776b587dbbd97
MajorChe <charithnanduri03@gmail.com>
2022-02-08 20:11:40 -0500

    add protect routes

--- money-well/src/App.js
+++ money-well/src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./home/Home";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Login from "./login/Login";
 import Signup from "./signup/Signup";
 
 const App = () => {
+  const { user, loggedIn } = useAuthContext();
+  const PrivateRoutes = () => {
+    return user ? <Outlet /> : <Navigate to={"/login"} />;
+  };
+
   return (
     <>
-    <Navbar/>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      {loggedIn && (
+        <>
+          <Navbar />
+          <Routes>
+            {!user && <Route path="/login" element={<Login />} />}
+            {!user && <Route path="/signup" element={<Signup />} />}
+            <Route element={<PrivateRoutes />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+            <Route path="/*" element={<Navigate to="/" />} />
+          </Routes>
+        </>
+      )}
     </>
   );
 };
